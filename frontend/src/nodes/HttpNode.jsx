66a569dc610e2728066e11ff9b4e574ab90da033
mdurka <m.durka@vertexagility.com>
2025-06-26 14:40:37 +0200

    nodes simple style change

--- frontend/src/nodes/HttpNode.jsx
+++ frontend/src/nodes/HttpNode.jsx
@@ -4,8 +4,21 @@ import httpIcon from '../assets/http_logo.png';
 
 function HttpNode({ data }) {
   return (
-    <div style={{ background: '#2c2c2c', color: '#fff', border: '1px solid #fa6926', borderRadius: 6, padding: 10 }}>
-      <img src={httpIcon} alt="HTTP" style={{ width: 40, height: 40 }} />
+    <div
+      style={{
+        background: '#2c2c2c',
+        color: '#fff',
+        border: '1px solid #fa6926',
+        borderRadius: 6,
+        padding: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minWidth: 100,
+      }}
+    >
+      <img src={httpIcon} alt="HTTP" style={{ width: 40, height: 40, marginBottom: 5 }} />
       <div>{data.label}</div>
       <Handle type="target" position={Position.Left} style={{ background: '#fa6926' }} />
       <Handle type="source" position={Position.Right} style={{ background: '#fa6926' }} />
