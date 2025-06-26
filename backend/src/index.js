c8098bf102effa6f4f1b2871b2737b3fe98fef57
mdurka <m.durka@vertexagility.com>
2025-06-26 11:33:14 +0200

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('CORVUS backend is running!');
+});
+
+app.listen(3001, () => {
+  console.log('🚀 Backend running on http://localhost:3001');
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to CORVUS</h1>
+      <p>Start building your node-based workflows here.</p>
+    </div>
+  );
+}
+
+export default App;
